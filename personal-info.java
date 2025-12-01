cc8702c4ac6211eebf60e765d00c34c965355e08
Aya Breaka <ayarefatbreaka@gmail.com>
2025-12-01 22:50:09 +0200

    Update personal-info.java

--- personal-info.java
+++ personal-info.java
@@ -1,1 +1,13 @@
+public class PersonalInfo {
+    public static void main(String[] args) {
+        String name = "Aya Refat Breaka";
+        String studentID = "220210776";
+        String major = "Computer Engineering";
+        String university = "Islamic University";
 
+        System.out.println("My Name: " + name);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Major: " + major);
+        System.out.println("University: " + university);
+    }
+}
