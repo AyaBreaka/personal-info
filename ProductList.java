f996081d2bd4734231493ddfacf22ec8f8e34ebd
Aya Breaka <ayarefatbreaka@gmail.com>
2025-12-01 23:23:09 +0200

    ProductList.java

--- ProductList.java
+++ ProductList.java
@@ -5,9 +5,14 @@ public class ProductList {
                              "Headphones", 
                              "Smartwatch", 
                              "Tablet"};
-        
+        double[] prices = {1500.0, 
+                           800.0, 
+                           150.0, 
+                           200.0, 
+                           400.0};
+
         for (int i = 0; i < products.length; i++) {
-            System.out.println((i + 1) + ". " + products[i]);
+            System.out.println((i + 1) + ". " + products[i] + " - Price: $" +   prices[i]);
         }
     }
-}
+} 
